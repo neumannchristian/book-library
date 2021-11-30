7bee716ed8b9709e802bf0c4cdbe8175103d64a6
neumannchristian <neumann.christian@protonmail.com>
2021-11-30 17:35:14 +0100

    Implement addBookToLibrary function

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let myLibrary = [];
+
 function Book(title, author, pages, haveRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
 
+Book.prototype.info = function () {
+  return `${title} by ${author}, ${pages} pages, ${
+    haveRead ? "already read" : "not read yet"
+  }`;
+}
 
+function addBookToLibrary(title, author, pages, haveRead) {
+  let newBook = new Book(title, author, pages, haveRead);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+console.log(myLibrary); 
 
