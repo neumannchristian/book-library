687fdcb1fa2c32141e0c5819ac3b2872ca793e33
neumannchristian <neumann.christian@protonmail.com>
2021-12-06 20:04:33 +0100

    Implement basic library view; list items

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
+const bookDisplay = document.querySelector("#book-display");
+
 let myLibrary = [];
 
-function Book(title, author, pages, haveRead) {
+function Book(title, author, totalPages, haveRead) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
+  this.totalPages = totalPages;
+  this.haveRead = haveRead;
 }
 
 Book.prototype.info = function () {
   return `${title} by ${author}, ${pages} pages, ${
     haveRead ? "already read" : "not read yet"
   }`;
-}
+};
 
 function addBookToLibrary(title, author, pages, haveRead) {
   let newBook = new Book(title, author, pages, haveRead);
   myLibrary.push(newBook);
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("Die Verwandlung", "Franz Kafka", 212, true);
+addBookToLibrary("The Sun Also Rises", "Ernest Miller Hemingway", 224, false);
+addBookToLibrary("The Constant Wife", "W. Somerset Maugham", 123, false);
+addBookToLibrary("It", "Stephen King", 128, false);
 
-console.log(myLibrary); 
+function populateBookDisplay() {
+  myLibrary.forEach((e, i) => {
+    let bookCard = document.createElement("div");
+    let bookCardTitle = document.createElement("div");
+    let bookCardAuthor = document.createElement("div");
+    let bookCardTotalPages = document.createElement("div");
+    let bookCardHaveRead = document.createElement("div");
 
+    bookCard.dataset.index = i;
+    bookCard.classList.add("book-card");
+
+    bookCardTitle.classList.add("card-book-title");
+    bookCardTitle.textContent = e.title;
+    
+    bookCardAuthor.textContent = e.author;
+    
+    bookCardTotalPages.textContent = e.totalPages;
+
+    bookCardHaveRead.textContent = e.haveRead;
+    console.log(e.haveRead);
+
+    bookCard.appendChild(bookCardTitle);
+    bookCard.appendChild(bookCardAuthor);
+    bookCard.appendChild(bookCardTotalPages);
+    bookCard.appendChild(bookCardHaveRead)
+    bookDisplay.appendChild(bookCard);
+  });
+}
+
+populateBookDisplay();
