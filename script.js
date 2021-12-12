43174558e1b9a484eaf7265f8aff688e5f11bf1c
neumannchristian <neumann.christian@protonmail.com>
2021-12-12 23:31:50 +0100

    Render and add books

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const bookDisplay = document.querySelector("#book-display");
+const bookOpenAddToLibraryModalBtn = document.querySelector(
+  "#book-openAddToLibraryModal"
+);
+const modal = document.querySelector("#modal");
+const modalCloseBtn = document.querySelector(
+  "#AddToLibraryWrapper span:first-child"
+);
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const totalPagesInput = document.querySelector("#totalPagesInput");
+const addToLibraryBtn = document.querySelector("#addToLibraryBtn");
 
 let myLibrary = [];
 
@@ -15,44 +26,83 @@ Book.prototype.info = function () {
   }`;
 };
 
-function addBookToLibrary(title, author, pages, haveRead) {
+function addBookToLibraryArray(title, author, pages, haveRead) {
   let newBook = new Book(title, author, pages, haveRead);
   myLibrary.push(newBook);
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
-addBookToLibrary("Die Verwandlung", "Franz Kafka", 212, true);
-addBookToLibrary("The Sun Also Rises", "Ernest Miller Hemingway", 224, false);
-addBookToLibrary("The Constant Wife", "W. Somerset Maugham", 123, false);
-addBookToLibrary("It", "Stephen King", 128, false);
+bookOpenAddToLibraryModalBtn.addEventListener("click", function () {
+  modal.style.display = "grid";
+});
+
+modalCloseBtn.addEventListener("click", function (event) {
+  modal.style.display = "none";
+});
+
+addToLibraryBtn.addEventListener("click", function () {
+  addBookToLibraryArray(
+    titleInput.value,
+    authorInput.value,
+    totalPagesInput.value,
+    true
+  );
+  modal.style.display = "none";
+  resetBookDisplay();
+  populateBookDisplay();
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+function resetBookDisplay() {
+  let childCount = bookDisplay.childElementCount;
+  for (let i = childCount - 1; i > 0; i--) {
+    bookDisplay.children[i].remove();
+  }
+}
+
+addBookToLibraryArray("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibraryArray("Die Verwandlung", "Franz Kafka", 212, true);
+addBookToLibraryArray(
+  "The Sun Also Rises",
+  "Ernest Miller Hemingway",
+  224,
+  false
+);
+addBookToLibraryArray("The Constant Wife", "W. Somerset Maugham", 123, false);
+addBookToLibraryArray("It", "Stephen King", 128, false);
+
+function addBookToBookDisplay(Book, i) {
+  let bookCard = document.createElement("div");
+  let bookCardTitle = document.createElement("div");
+  let bookCardAuthor = document.createElement("div");
+  let bookCardTotalPages = document.createElement("div");
+  let bookCardHaveRead = document.createElement("div");
+
+  bookCard.dataset.index = i;
+  bookCard.classList.add("book-card");
+
+  bookCardTitle.classList.add("card-book-title");
+  bookCardTitle.textContent = Book.title;
+
+  bookCardAuthor.textContent = Book.author;
+
+  bookCardTotalPages.textContent = Book.totalPages;
+
+  bookCardHaveRead.textContent = Book.haveRead;
+
+  bookCard.appendChild(bookCardTitle);
+  bookCard.appendChild(bookCardAuthor);
+  bookCard.appendChild(bookCardTotalPages);
+  bookCard.appendChild(bookCardHaveRead);
+  bookDisplay.appendChild(bookCard);
+}
 
 function populateBookDisplay() {
-  myLibrary.forEach((e, i) => {
-    let bookCard = document.createElement("div");
-    let bookCardTitle = document.createElement("div");
-    let bookCardAuthor = document.createElement("div");
-    let bookCardTotalPages = document.createElement("div");
-    let bookCardHaveRead = document.createElement("div");
-
-    bookCard.dataset.index = i;
-    bookCard.classList.add("book-card");
-
-    bookCardTitle.classList.add("card-book-title");
-    bookCardTitle.textContent = e.title;
-    
-    bookCardAuthor.textContent = e.author;
-    
-    bookCardTotalPages.textContent = e.totalPages;
-
-    bookCardHaveRead.textContent = e.haveRead;
-    console.log(e.haveRead);
-
-    bookCard.appendChild(bookCardTitle);
-    bookCard.appendChild(bookCardAuthor);
-    bookCard.appendChild(bookCardTotalPages);
-    bookCard.appendChild(bookCardHaveRead)
-    bookDisplay.appendChild(bookCard);
-  });
+  myLibrary.forEach((e, i) => addBookToBookDisplay(e, i));
 }
 
 populateBookDisplay();
